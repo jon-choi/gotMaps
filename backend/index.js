6b0079edf0ce41f3f17c73bb2af8adbbb92d1f53
Jonathan Choi <jonchoi11@gmail.com>
2021-11-24 14:02:17 -0700

    install dotenv

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/myapp", { useNewUrlParser: true });
+
 app.listen(8800, () => {
   console.log("backend server is running!");
 });
