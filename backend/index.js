fa3604fef78d126af44bbe8cb113568eef72347d
Jonathan Choi <jonchoi11@gmail.com>
2021-11-25 12:54:52 -0700

    add users routes to index and change port number

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
 
 const app = express();
 
@@ -20,7 +21,8 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
 
-app.listen(8800, () => {
-  console.log("backend server is running on Port: 8800!");
+app.listen(8080, () => {
+  console.log("backend server is running on Port: 8080!");
 });
