40b6879f32f3f53543693050294c973d3ca930fd
Jonathan Choi <jonchoi11@gmail.com>
2021-11-25 10:10:16 -0700

    build register route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,3 +1,26 @@
 const User = require("../models/User");
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    // Generate a new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create a new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // Save user & respond
+    const user = await newUser.save();
+    res.status(200).json(user._id);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
