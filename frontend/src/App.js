a733186cdf9412fa3733d0c310016fe50ca50e10
Jonathan Choi <jonchoi11@gmail.com>
2021-11-25 11:23:55 -0700

    add react marker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
 
 const App = () => {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
+    width: "100vw",
+    height: "100vh",
+    latitude: 48.4284,
+    longitude: -123.3656,
+    zoom: 12,
   });
 
   return (
     <div className="App">
       <ReactMapGL
         {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      />
+      >
+        <Marker
+          latitude={48.428244}
+          longitude={-123.371967}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <div>You are here</div>
+        </Marker>
+      </ReactMapGL>
     </div>
   );
 };
