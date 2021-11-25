3a3233f5706876a3690200a4ca5e190085fe07f0
Jonathan Choi <jonchoi11@gmail.com>
2021-11-25 11:08:52 -0700

    install react-map, insert data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
 const App = () => {
-  return <div className="App">hello</div>;
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
+  return (
+    <div className="App">
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
+    </div>
+  );
 };
 
 export default App;
