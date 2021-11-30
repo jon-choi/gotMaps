e18c969188157a3950dca0c500d75f7930879d45
Jonathan Choi <jonchoi11@gmail.com>
2021-11-30 11:23:52 -0700

    import useRef

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,15 @@
 import './register.css';
 import { Room } from '@material-ui/icons';
+import { useState, useRef } from 'react';
 
 export default function Register() {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+
   return (
     <div className="registerContainer">
         <div className="logo">
@@ -13,6 +21,11 @@ export default function Register() {
           <input type="text" placeholder="email" />
           <input type="text" placeholder="password" />
           <button className="registerButton">Register</button>
+          {success && (<span className="success">You can now login!</span>)}
+          {error && (<span className="failure">Oops! Something went wrong!</span>)}
+
+          
+          
         </form>
     </div>
   )
